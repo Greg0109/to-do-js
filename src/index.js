2b4877b68e2d1582a11911fb001a11fe056ef562
Greg0109 <greg.rabago@gmail.com>
2021-07-15 09:00:57 +0200

    changed to html list instead of div, use checkbox

--- src/index.js
+++ src/index.js
@@ -23,20 +23,24 @@ function todolist() {
   element.classList.add('todolist');
 
   todoarray.forEach((todoitem) => {
-    const item = document.createElement('div');
+    const item = document.createElement('li');
     item.classList.add('todo-item');
     const title = document.createElement('p');
     title.classList.add('todo-title');
     title.textContent = todoitem.description;
     if (todoitem.completed) {
-      const completed = document.createElement('i');
-      completed.classList.add('far');
-      completed.classList.add('fa-check-square');
+      const completed = document.createElement('input');
+      completed.type = "checkbox";
+      completed.name = "completed";
+      completed.value = "value";
+      completed.id = "id";
       title.appendChild(completed);
     } else {
-      const completed = document.createElement('i');
-      completed.classList.add('far');
-      completed.classList.add('fa-square');
+      const completed = document.createElement('input');
+      completed.type = "checkbox";
+      completed.name = "completed";
+      completed.value = "value";
+      completed.id = "id";
       title.appendChild(completed);
     }
     item.appendChild(title);
