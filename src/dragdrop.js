1f73ac771837e8d3beb219e8422ff44d6899d4fe
Greg0109 <greg.rabago@gmail.com>
2021-07-15 09:46:06 +0200

    Fixed localstorage

--- src/dragdrop.js
+++ src/dragdrop.js
@@ -1,51 +1,25 @@
-const initialStorageData = JSON.parse(localStorage.getItem('todolist')) || [
-    {
-      index: 0,
-      completed: false,
-      description: 'Pick up the trash',
-    },
-    {
-      index: 1,
-      completed: true,
-      description: 'Create To-Do list milestone 1',
-    },
-    {
-      index: 2,
-      completed: false,
-      description: 'Take the dog for a walk',
-    },
-  ];
-
-const localStorage = {
-    retrievelist: () => {
-        const list = JSON.parse(localStorage.getItem('todolist')) || [];
-        return list;
-    }
-
-    saveList: (changes) => {
-        const list = this.retrievelist();
-        list.push(changes);
-        localStorage.setItem("todolist", JSON.stringify(list));
-    }
-}
-
 class dragdrop {
-    constructor(index) {
-        this.index = index;
-    }
-    drag (i) {
-        draggedIndex = i;
-    }
+  constructor(index) {
+    this.index = index;
+  }
 
-    dragover (element) {
-        element.preventDefault();
-    }
-    
-    drop (i) {
+  drag(i) {
+    draggedIndex = i;
+  }
 
-    }
+  dragover(element) {
+    element.preventDefault();
+  }
 
-    dragstart (element) {
+  drop(i) {
+    const list = localStorageConst.retrievelist();
+    const tempChange = list[this.draggedIndex];
+    list[this.draggedIndex] = list[i];
+    list[i] = tempChange;
+    localStorageConst.saveList(list);
+  }
 
-    }
+  dragstart(element) {
+
+  }
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import './style.css';
+import localStorageConst from './localstorage';
 
 function todolist() {
-  const todoarray = [
-    {
-      index: 0,
-      completed: false,
-      description: 'Pick up the trash',
-    },
-    {
-      index: 1,
-      completed: true,
-      description: 'Create To-Do list milestone 1',
-    },
-    {
-      index: 2,
-      completed: false,
-      description: 'Take the dog for a walk',
-    },
-  ];
+  const todoarray = localStorageConst.retrievelist();
 
   const element = document.createElement('div');
   element.classList.add('todolist');
@@ -30,17 +15,17 @@ function todolist() {
     title.textContent = todoitem.description;
     if (todoitem.completed) {
       const completed = document.createElement('input');
-      completed.type = "checkbox";
-      completed.name = "completed";
-      completed.value = "value";
-      completed.id = "id";
+      completed.type = 'checkbox';
+      completed.name = 'completed';
+      completed.value = 'value';
+      completed.id = 'id';
       title.appendChild(completed);
     } else {
       const completed = document.createElement('input');
-      completed.type = "checkbox";
-      completed.name = "completed";
-      completed.value = "value";
-      completed.id = "id";
+      completed.type = 'checkbox';
+      completed.name = 'completed';
+      completed.value = 'value';
+      completed.id = 'id';
       title.appendChild(completed);
     }
     item.appendChild(title);
